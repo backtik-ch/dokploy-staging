1547be227177be1d2cd001ce5bb3acffcff030b5
vincent-tarrit <91567748+vincent-tarrit@users.noreply.github.com>
2026-02-09 23:14:04 +0100

    feat: routes

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post('/{projectID}', [DeployController::class, 'deploy'])->middleware('auth:sanctum');
-Route::delete('/{projectID}', [DeployController::class, 'delete'])->middleware('auth:sanctum');
+Route::post('/deploy', [DeployController::class, 'deploy'])->middleware('auth:sanctum');
+Route::delete('/remove', [DeployController::class, 'delete'])->middleware('auth:sanctum');
